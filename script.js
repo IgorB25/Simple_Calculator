26321984b6833b13bb86dd5a1c772842b49feed8
Igor <igorbulka5@gmail.com>
2022-03-14 19:58:07 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+ function add(FirstNum,SecondNum){
+    FirstNum += SecondNum;
+    console.log(FirstNum);
+}
+add(2,4)
+
+
+function subtract(FirstNum,SecondNum){
+    FirstNum -= SecondNum;
+    console.log(FirstNum);
+}
+subtract(5,6)
+
+
+function multiply(FirstNum,SecondNum){
+    FirstNum *= SecondNum;
+    console.log(FirstNum)
+}
+multiply(3,10)
+
+function divide(FirstNum,SecondNum){
+    FirstNum /= SecondNum;
+    console.log(FirstNum)
+}
+divide(9,3)
