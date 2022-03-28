f5a5b2c656a7ed55f93e443b1a3cd050979a3c8e
Igor <igorbulka5@gmail.com>
2022-03-28 19:00:32 +0200

    finished main functions of calculator

--- script.js
+++ script.js
@@ -3,24 +3,49 @@
     FirstNum += SecondNum;
     console.log(FirstNum);
 }
-add(2,4)
+//add(2,4)
 
 
 function subtract(FirstNum,SecondNum){
     FirstNum -= SecondNum;
     console.log(FirstNum);
 }
-subtract(5,6)
+//subtract(7,6)
 
 
 function multiply(FirstNum,SecondNum){
     FirstNum *= SecondNum;
     console.log(FirstNum)
 }
-multiply(3,10)
+//multiply(3,10)
 
 function divide(FirstNum,SecondNum){
     FirstNum /= SecondNum;
     console.log(FirstNum)
 }
-divide(9,3)
+//divide(9,3)
+
+function operate(operator,FirstNum,SecondNum){
+
+    switch(operator){
+
+        case "+":
+            add(FirstNum,SecondNum);
+            break;
+
+        case "-":
+            subtract(FirstNum,SecondNum);
+            break;
+
+        case "*":
+            multiply(FirstNum,SecondNum);
+            break;
+
+        case "/":
+            divide(FirstNum,SecondNum);
+            break;
+
+    }
+}
+
+//operate("/",7,2)
